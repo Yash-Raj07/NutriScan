374d54c46e3641caa9d93dfca8a5ae5972f926a6
ujjwal <uzzwal7505@gmail.com>
2024-12-22 09:25:40 +0530

    npm issue

--- src/Ingredientsubstitue.jsx
+++ src/Ingredientsubstitue.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import {
   Button,
   Grid,
